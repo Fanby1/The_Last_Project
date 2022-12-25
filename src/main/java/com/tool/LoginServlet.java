4e4f42e66edc39e7787cc739e35748da9f9d17e2
Angry_Cookie <fanby2014@126.com>
2022-12-25 17:00:41 +0800

    已知bug修复

--- src/main/java/com/tool/LoginServlet.java
+++ src/main/java/com/tool/LoginServlet.java
@@ -5,15 +5,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static com.tool.Constant.sql;
 import static com.tool.Constant.warName;
 
-public class loginServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         UserToken userToken = new UserToken(req);
